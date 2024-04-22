ccf8151a15aec1e8f48798c10eaeed5a65f20c19
hilmizul <netspytux@gmail.com>
2024-04-22 14:20:31 +0700

    perubahan di file home

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row">
     <div class="col-lg-12">
-      <h1>HOME</h1>
+      <h1>HOME.</h1>
       <h3>selamat datang, (username dan tipe user)</h3>
       <div v-if="user">
         {{ user.user_metadata.username }}
